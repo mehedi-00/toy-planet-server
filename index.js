d6626281ce8d6925c7a499a40fd1e2a35c035b8e
Mehedi <developermehedi128@gmail.com>
2023-05-21 19:52:53 +0600

    vercel json file added

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
 
@@ -120,7 +120,7 @@ async function run() {
             res.send(result);
         });
 
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
